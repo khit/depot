bf357e45b62c98cbc78a435eab665272bfada212
Kye Hittle <kye.hittle@gmail.com>
2011-07-07 02:41:44 -0500

    chapters 10 & 11 completed

--- db/migrate/20100927054051_combine_items_in_cart.rb
+++ db/migrate/20100927054051_combine_items_in_cart.rb
@@ -19,14 +19,14 @@ class CombineItemsInCart < ActiveRecord::Migration
 
   def self.down
     # split items with quantity > 1 into multiple items
-    LineItem.where("quantity > 1").each do |lineitem|
+    LineItem.where("quantity > 1").each do |line_item|
       # add individual items`
-      lineitem.quantity.times do
-        LineItem.create :cart_id => lineitem.cart_id, :product_id => lineitem.product_id, :quantity => 1
+      line_item.quantity.times do
+        LineItem.create :cart_id => line_item.cart_id, :product_id => line_item.product_id, :quantity => 1
       end
       
       # remove original line item
-      lineitem.destroy
+      line_item.destroy
     end
   end
 end
